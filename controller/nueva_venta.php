85ee1b83f87afc4c620c51aef761fbe7bc3e1556
luismi <luismi@pcrednet.com>
2015-08-05 13:07:57 +0200

    Creo modificaciones para seleccionar el grupo de cliente cuando se crea un nuevo cliente desde el formulario rápcio.

--- controller/nueva_venta.php
+++ controller/nueva_venta.php
@@ -49,6 +49,8 @@ class nueva_venta extends fs_controller
    public $results;
    public $serie;
    public $tipo;
+   public $grupo;
+
    
    public function __construct()
    {
@@ -63,6 +65,8 @@ class nueva_venta extends fs_controller
       $this->familia = new familia();
       $this->impuesto = new impuesto();
       $this->results = array();
+      $this->grupo = new grupo_clientes();
+
       
       if( isset($_REQUEST['tipo']) )
       {
@@ -121,10 +125,16 @@ class nueva_venta extends fs_controller
                   $this->cliente_s->codcliente = $this->cliente_s->get_new_codigo();
                   $this->cliente_s->nombre = $this->cliente_s->razonsocial = $_POST['nuevo_cliente'];
                   $this->cliente_s->cifnif = $_POST['nuevo_dni'];
+                  $this->cliente_s->codgrupo = $_POST['codgrupo'];
                   $this->cliente_s->save();
                }
             }
          }
+         
+         $this->cliente->codgrupo = NULL;
+         if($_POST['codgrupo'] != '---')
+            $this->cliente->codgrupo = $_POST['codgrupo'];
+         
          
          if($this->cliente_s)
          {
