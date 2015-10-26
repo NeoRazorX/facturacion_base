73aa45eb0106ed1ad8dc04ddd23b34dcbb70904a
NeoRazorX <neorazorx@gmail.com>
2015-10-26 11:34:22 +0100

    Mejorados los documentos simples para que muestren las columnas iva, re, e irpf cuando sea necesario.

--- model/agencia_transporte.php
+++ model/agencia_transporte.php
@@ -18,6 +18,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Agencia de transporte de mercancías.
+ */
 class agencia_transporte extends fs_model
 {
    /**
