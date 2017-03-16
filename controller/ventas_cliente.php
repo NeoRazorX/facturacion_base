c76ad2410219ab65e3ed9476ac099f0f35d9ad48
NeoRazorX <neorazorx@gmail.com>
2017-03-16 20:06:27 +0100

    Corregida la conversión de cliente a proveedores y vicebersa.

--- controller/ventas_cliente.php
+++ controller/ventas_cliente.php
@@ -80,222 +80,27 @@ class ventas_cliente extends fs_controller
          /// ¿Hay que hacer algo más?
          if( isset($_GET['delete_cuenta']) ) /// eliminar cuenta bancaria
          {
-            $cuenta = $this->cuenta_banco->get($_GET['delete_cuenta']);
-            if($cuenta)
-            {
-               if( $cuenta->delete() )
-               {
-                  $this->new_message('Cuenta bancaria eliminada correctamente.');
-               }
-               else
-                  $this->new_error_msg('Imposible eliminar la cuenta bancaria.');
-            }
-            else
-               $this->new_error_msg('Cuenta bancaria no encontrada.');
+            $this->delete_cuenta_banco();
          }
          else if( isset($_GET['delete_dir']) ) /// eliminar dirección
          {
-            $dir = new direccion_cliente();
-            $dir0 = $dir->get($_GET['delete_dir']);
-            if($dir0)
-            {
-               if( $dir0->delete() )
-               {
-                  $this->new_message('Dirección eliminada correctamente.');
-               }
-               else
-                  $this->new_error_msg('Imposible eliminar la dirección.');
-            }
-            else
-               $this->new_error_msg('Dirección no encontrada.');
+            $this->delete_direccion();
          }
          else if( isset($_POST['coddir']) ) /// añadir/modificar dirección
          {
-            $dir = new direccion_cliente();
-            if($_POST['coddir'] != '')
-            {
-               $dir = $dir->get($_POST['coddir']);
-            }
-            $dir->apartado = $_POST['apartado'];
-            $dir->ciudad = $_POST['ciudad'];
-            $dir->codcliente = $this->cliente->codcliente;
-            $dir->codpais = $_POST['pais'];
-            $dir->codpostal = $_POST['codpostal'];
-            $dir->descripcion = $_POST['descripcion'];
-            $dir->direccion = $_POST['direccion'];
-            $dir->domenvio = isset($_POST['direnvio']);
-            $dir->domfacturacion = isset($_POST['dirfact']);
-            $dir->provincia = $_POST['provincia'];
-            if( $dir->save() )
-            {
-               $this->new_message("Dirección guardada correctamente.");
-            }
-            else
-               $this->new_message("¡Imposible guardar la dirección!");
+            $this->edit_direccion();
          }
          else if( isset($_POST['iban']) ) /// añadir/modificar iban
          {
-            if( isset($_POST['codcuenta']) )
-            {
-               $cuentab = $this->cuenta_banco->get($_POST['codcuenta']);
-            }
-            else
-            {
-               $cuentab = new cuenta_banco_cliente();
-               $cuentab->codcliente = $_POST['codcliente'];
-            }
-            
-            $cuentab->descripcion = $_POST['descripcion'];
-            $cuentab->iban = $_POST['iban'];
-            $cuentab->swift = $_POST['swift'];
-            $cuentab->principal = isset($_POST['principal']);
-            $cuentab->fmandato = NULL;
-            
-            if( isset($_POST['fmandato']) )
-            {
-               if($_POST['fmandato'] != '')
-               {
-                  $cuentab->fmandato = $_POST['fmandato'];
-               }
-            }
-            
-            if( $cuentab->save() )
-            {
-               $this->new_message('Cuenta bancaria guardada correctamente.');
-            }
-            else
-               $this->new_error_msg('Imposible guardar la cuenta bancaria.');
+            $this->edit_cuenta_banco();
          }
          else if( isset($_POST['codcliente']) ) /// modificar cliente
          {
-            $this->cliente->nombre = $_POST['nombre'];
-            $this->cliente->razonsocial = $_POST['razonsocial'];
-            $this->cliente->tipoidfiscal = $_POST['tipoidfiscal'];
-            $this->cliente->cifnif = $_POST['cifnif'];
-            $this->cliente->telefono1 = $_POST['telefono1'];
-            $this->cliente->telefono2 = $_POST['telefono2'];
-            $this->cliente->fax = $_POST['fax'];
-            $this->cliente->web = $_POST['web'];
-            $this->cliente->email = $_POST['email'];
-            $this->cliente->observaciones = $_POST['observaciones'];
-            $this->cliente->codpago = $_POST['codpago'];
-            $this->cliente->coddivisa = $_POST['coddivisa'];
-            $this->cliente->regimeniva = $_POST['regimeniva'];
-            $this->cliente->recargo = isset($_POST['recargo']);
-            $this->cliente->debaja = isset($_POST['debaja']);
-            $this->cliente->personafisica = isset($_POST['personafisica']);
-            $this->cliente->diaspago = $_POST['diaspago'];
-            
-            $this->cliente->codserie = NULL;
-            if($_POST['codserie'] != '')
-            {
-               $this->cliente->codserie = $_POST['codserie'];
-            }
-            
-            $this->cliente->codagente = NULL;
-            if($_POST['codagente'] != '')
-            {
-               $this->cliente->codagente = $_POST['codagente'];
-            }
-            
-            $this->cliente->codgrupo = NULL;
-            if($_POST['codgrupo'] != '')
-            {
-               $this->cliente->codgrupo = $_POST['codgrupo'];
-            }
-            
-            if( $this->cliente->save() )
-            {
-               $this->new_message("Datos del cliente modificados correctamente.");
-               $this->propagar_cifnif();
-            }
-            else
-               $this->new_error_msg("¡Imposible modificar los datos del cliente!");
+            $this->modificar();
          }
          else if( isset($_GET['convertir']) ) 
          {
-         	$proveedor = new proveedor();
-         	
-         	/* datos generales */
-         	$proveedor->nombre = $this->cliente->nombre;
-         	$proveedor->razonsocial = $this->cliente->razonsocial;
-         	$proveedor->tipoidfiscal = $this->cliente->tipoidfiscal;
-         	$proveedor->cifnif = $this->cliente->cifnif;
-         	$proveedor->telefono1 = $this->cliente->telefono1;
-         	$proveedor->telefono2 = $this->cliente->telefono2;
-         	$proveedor->fax = $this->cliente->fax;
-         	$proveedor->web = $this->cliente->web;
-         	$proveedor->email = $this->cliente->email;
-         	$proveedor->observaciones = $this->cliente->observaciones;
-         	$proveedor->codpago = $this->cliente->codpago;
-         	$proveedor->coddivisa = $this->cliente->coddivisa;
-         	$proveedor->regimeniva = $this->cliente->regimeniva;
-         	$proveedor->recargo = $this->cliente->recargo;
-         	$proveedor->debaja = $this->cliente->debaja;
-         	$proveedor->fechabaja = $this->cliente->fechabaja;
-         	$proveedor->personafisica = $this->cliente->personafisica;
-         	$proveedor->diaspago = $this->cliente->diaspago;
-         	$proveedor->codserie = $this->cliente->codserie;
-         	$proveedor->codagente = $this->cliente->codagente;         	
-         	$proveedor->codgrupo = $this->cliente->codgrupo;         	
-         	$proveedor->codcliente = $this->cliente->codcliente;
-         	
-         	/* salvamos el proveedor */
-         	$proveedor_ok = true;
-         	if( $proveedor->save() )
-         	{
-         		$this->cliente->codproveedor = $proveedor->codproveedor;
-         		$proveedor_ok = $proveedor_ok && $this->cliente->save();
-         	}
-         	else
-         		$proveedor_ok = false;
-         	
-         	/* sólo si fue bien el salvado */
-         	if ($proveedor_ok)
-         	{
-	         	/* cuentas de banco */
-	         	$cuentas_banco_cli = $this->cuenta_banco->all_from_cliente($this->cliente->codcliente);
-	         	$cbancos_ok = true;
-	         	foreach ($cuentas_banco_cli as $c)
-	         	{
-	         		$c_banco_proveedor = new cuenta_banco_proveedor();
-	         		$c_banco_proveedor->codcuenta = $c->codcuenta;
-	         		$c_banco_proveedor->codproveedor = $proveedor->codproveedor;
-	         		$c_banco_proveedor->descripcion = $c->descripcion;
-	         		$c_banco_proveedor->iban = $c->iban;
-	         		$c_banco_proveedor->swift = $c->swift;
-	  				$c_banco_proveedor->principal = $c->principal;
-	   				$cbancos_ok = $cbancos_ok && $c_banco_proveedor->save(); 
-	         	}
-	         	
-	         	/* direcciones */
-	         	$direcc_cli = $this->cliente->get_direcciones();
-	         	$direcc_ok = true;
-	         	foreach ($direcc_cli as $d)
-	         	{
-	         		$direcc_pro = new direccion_proveedor();
-	         		$direcc_pro->codproveedor = $proveedor->codproveedor;
-	         		$direcc_pro->codpais = $d->codpais;
-	         		$direcc_pro->apartado = $d->apartado;
-	         		$direcc_pro->provincia = $d->provincia;
-	         		$direcc_pro->ciudad = $d->ciudad;
-	         		$direcc_pro->codpostal = $d->codpostal;
-	         		$direcc_pro->direccion = $d->direccion;
-	         		$direcc_pro->direccionppal = $d->direccionppal;
-	         		$direcc_pro->descripcion = $d->descripcion;         		
-	         		$direcc_ok = $direcc_ok && $direcc_pro->save();
-	         	}
-         	}
-         	
-         	/* resultado */
-         	if( $proveedor_ok )
-         	{
-         		$txtadded = $cbancos_ok ? "" : ". No se pudieron copiar todas las cuentas bancarias";
-         		$txtadded .= $direcc_ok ? "" : ". No se pudieron copiar todas las direcciones";
-         		$this->new_message("Conversión correcta. Proveedor ".$proveedor->codproveedor.$txtadded);
-         	}
-         	else
-         		$this->new_error_msg("¡Imposible realizar la conversión!");
+         	$this->convertir();
          }
       }
       else
@@ -316,6 +121,208 @@ class ventas_cliente extends fs_controller
       }
       else
          return $this->ppage->url();
+   }
+   
+   private function modificar()
+   {
+      $this->cliente->nombre = $_POST['nombre'];
+      $this->cliente->razonsocial = $_POST['razonsocial'];
+      $this->cliente->tipoidfiscal = $_POST['tipoidfiscal'];
+      $this->cliente->cifnif = $_POST['cifnif'];
+      $this->cliente->telefono1 = $_POST['telefono1'];
+      $this->cliente->telefono2 = $_POST['telefono2'];
+      $this->cliente->fax = $_POST['fax'];
+      $this->cliente->web = $_POST['web'];
+      $this->cliente->email = $_POST['email'];
+      $this->cliente->observaciones = $_POST['observaciones'];
+      $this->cliente->codpago = $_POST['codpago'];
+      $this->cliente->coddivisa = $_POST['coddivisa'];
+      $this->cliente->regimeniva = $_POST['regimeniva'];
+      $this->cliente->recargo = isset($_POST['recargo']);
+      $this->cliente->debaja = isset($_POST['debaja']);
+      $this->cliente->personafisica = isset($_POST['personafisica']);
+      $this->cliente->diaspago = $_POST['diaspago'];
+      
+      $this->cliente->codserie = NULL;
+      if($_POST['codserie'] != '')
+      {
+         $this->cliente->codserie = $_POST['codserie'];
+      }
+      
+      $this->cliente->codagente = NULL;
+      if($_POST['codagente'] != '')
+      {
+         $this->cliente->codagente = $_POST['codagente'];
+      }
+      
+      $this->cliente->codgrupo = NULL;
+      if($_POST['codgrupo'] != '')
+      {
+         $this->cliente->codgrupo = $_POST['codgrupo'];
+      }
+      
+      if( $this->cliente->save() )
+      {
+         $this->new_message("Datos del cliente modificados correctamente.");
+         $this->propagar_cifnif();
+      }
+      else
+         $this->new_error_msg("¡Imposible modificar los datos del cliente!");
+   }
+   
+   private function edit_cuenta_banco()
+   {
+      if( isset($_POST['codcuenta']) )
+      {
+         $cuentab = $this->cuenta_banco->get($_POST['codcuenta']);
+      }
+      else
+      {
+         $cuentab = new cuenta_banco_cliente();
+         $cuentab->codcliente = $_POST['codcliente'];
+      }
+      
+      $cuentab->descripcion = $_POST['descripcion'];
+      $cuentab->iban = $_POST['iban'];
+      $cuentab->swift = $_POST['swift'];
+      $cuentab->principal = isset($_POST['principal']);
+      $cuentab->fmandato = NULL;
+      
+      if( isset($_POST['fmandato']) )
+      {
+         if($_POST['fmandato'] != '')
+         {
+            $cuentab->fmandato = $_POST['fmandato'];
+         }
+      }
+      
+      if( $cuentab->save() )
+      {
+         $this->new_message('Cuenta bancaria guardada correctamente.');
+      }
+      else
+         $this->new_error_msg('Imposible guardar la cuenta bancaria.');
+   }
+   
+   private function delete_cuenta_banco()
+   {
+      $cuenta = $this->cuenta_banco->get($_GET['delete_cuenta']);
+      if($cuenta)
+      {
+         if( $cuenta->delete() )
+         {
+            $this->new_message('Cuenta bancaria eliminada correctamente.');
+         }
+         else
+            $this->new_error_msg('Imposible eliminar la cuenta bancaria.');
+      }
+      else
+         $this->new_error_msg('Cuenta bancaria no encontrada.');
+   }
+   
+   private function edit_direccion()
+   {
+      $dir = new direccion_cliente();
+      if($_POST['coddir'] != '')
+      {
+         $dir = $dir->get($_POST['coddir']);
+      }
+      $dir->apartado = $_POST['apartado'];
+      $dir->ciudad = $_POST['ciudad'];
+      $dir->codcliente = $this->cliente->codcliente;
+      $dir->codpais = $_POST['pais'];
+      $dir->codpostal = $_POST['codpostal'];
+      $dir->descripcion = $_POST['descripcion'];
+      $dir->direccion = $_POST['direccion'];
+      $dir->domenvio = isset($_POST['direnvio']);
+      $dir->domfacturacion = isset($_POST['dirfact']);
+      $dir->provincia = $_POST['provincia'];
+      if( $dir->save() )
+      {
+         $this->new_message("Dirección guardada correctamente.");
+      }
+      else
+         $this->new_message("¡Imposible guardar la dirección!");
+   }
+   
+   private function delete_direccion()
+   {
+      $dir = new direccion_cliente();
+      $dir0 = $dir->get($_GET['delete_dir']);
+      if($dir0)
+      {
+         if( $dir0->delete() )
+         {
+            $this->new_message('Dirección eliminada correctamente.');
+         }
+         else
+            $this->new_error_msg('Imposible eliminar la dirección.');
+      }
+      else
+         $this->new_error_msg('Dirección no encontrada.');
+   }
+   
+   private function convertir()
+   {
+      $proveedor = new proveedor();
+      $proveedor->nombre = $this->cliente->nombre;
+      $proveedor->razonsocial = $this->cliente->razonsocial;
+      $proveedor->tipoidfiscal = $this->cliente->tipoidfiscal;
+      $proveedor->cifnif = $this->cliente->cifnif;
+      $proveedor->telefono1 = $this->cliente->telefono1;
+      $proveedor->telefono2 = $this->cliente->telefono2;
+      $proveedor->fax = $this->cliente->fax;
+      $proveedor->web = $this->cliente->web;
+      $proveedor->email = $this->cliente->email;
+      $proveedor->observaciones = $this->cliente->observaciones;
+      $proveedor->codpago = $this->cliente->codpago;
+      $proveedor->coddivisa = $this->cliente->coddivisa;
+      $proveedor->regimeniva = $this->cliente->regimeniva;
+      $proveedor->personafisica = $this->cliente->personafisica;
+      $proveedor->codserie = $this->cliente->codserie;
+      $proveedor->codcliente = $this->cliente->codcliente;
+      
+      $proveedor_ok = TRUE;
+      if( $proveedor->save() )
+      {
+         $this->cliente->codproveedor = $proveedor->codproveedor;
+         $proveedor_ok = $this->cliente->save();
+      }
+      else
+         $proveedor_ok = FALSE;
+      
+      if($proveedor_ok)
+      {
+         /* cuentas de banco */
+         foreach($this->cuenta_banco->all_from_cliente($this->cliente->codcliente) as $c)
+	      {
+            $c_banco_proveedor = new cuenta_banco_proveedor();
+            $c_banco_proveedor->codproveedor = $proveedor->codproveedor;
+            $c_banco_proveedor->descripcion = $c->descripcion;
+            $c_banco_proveedor->iban = $c->iban;
+            $c_banco_proveedor->swift = $c->swift;
+            $c_banco_proveedor->save();
+	      }
+         
+         /* direcciones */
+	      foreach($this->cliente->get_direcciones() as $d)
+	      {
+            $direcc_pro = new direccion_proveedor();
+	         $direcc_pro->codproveedor = $proveedor->codproveedor;
+	         $direcc_pro->codpais = $d->codpais;
+	         $direcc_pro->apartado = $d->apartado;
+	         $direcc_pro->provincia = $d->provincia;
+	         $direcc_pro->ciudad = $d->ciudad;
+	         $direcc_pro->codpostal = $d->codpostal;
+	         $direcc_pro->direccion = $d->direccion;
+            $direcc_pro->descripcion = $d->descripcion;
+            $direcc_pro->save();
+	      }
+         
+         $this->new_message('Proveedor creado correctamente.');
+      }
+      else
+         $this->new_error_msg("¡Imposible crear el proveedor!");
    }
    
    /*
