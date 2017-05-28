aa766162b4e6f7c7c3e0a6463d55ab1462a19ace
NeoRazorX <neorazorx@gmail.com>
2017-05-28 12:08:57 +0200

    - El listado de proveedores ya no muestra los proveedores dados de baja, a menos que se marque el filtro. - Modificadas las interfaces de los agrupadores de albaranes para marcar/desmarcar todo pulsando el checkbox de la columna.

--- controller/compras_proveedores.php
+++ controller/compras_proveedores.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of facturacion_base
  * Copyright (C) 2013-2017  Carlos Garcia Gomez  neorazorx@gmail.com
@@ -20,8 +21,9 @@
 require_once 'plugins/facturacion_base/extras/fbase_controller.php';
 require_model('pais.php');
 
-class compras_proveedores extends fbase_controller
-{
+class compras_proveedores extends fbase_controller {
+
+   public $debaja;
    public $num_resultados;
    public $offset;
    public $orden;
@@ -29,58 +31,47 @@ class compras_proveedores extends fbase_controller
    public $proveedor;
    public $resultados;
    public $tipo;
-   public $debaja;
-   
-   public function __construct()
-   {
+
+   public function __construct() {
       parent::__construct(__CLASS__, 'Proveedores / Acreedores', 'compras');
    }
-   
-   protected function private_core()
-   {
+
+   protected function private_core() {
       parent::private_core();
-      
+
       $this->pais = new pais();
       $this->proveedor = new proveedor();
-      
-      if( isset($_GET['delete']) )
-      {
+
+      if (isset($_GET['delete'])) {
          $this->eliminar_proveedor();
-      }
-      else if( isset($_POST['cifnif']) )
-      {
+      } else if (isset($_POST['cifnif'])) {
          $this->nuevo_proveedor();
       }
-      
+
       $this->ini_filters();
       $this->buscar();
    }
-   
-   private function ini_filters()
-   {
+
+   private function ini_filters() {
       $this->offset = 0;
-      if( isset($_GET['offset']) )
-      {
+      if (isset($_GET['offset'])) {
          $this->offset = intval($_GET['offset']);
       }
-      
+
       $this->orden = 'lower(nombre) ASC';
-      if( isset($_REQUEST['orden']) )
-      {
+      if (isset($_REQUEST['orden'])) {
          $this->orden = $_REQUEST['orden'];
       }
-      
+
       $this->tipo = '';
-      if( isset($_REQUEST['tipo']) )
-      {
+      if (isset($_REQUEST['tipo'])) {
          $this->tipo = $_REQUEST['tipo'];
       }
-      
+
       $this->debaja = isset($_REQUEST['debaja']);
    }
-   
-   private function nuevo_proveedor()
-   {
+
+   private function nuevo_proveedor() {
       $proveedor = new proveedor();
       $proveedor->codproveedor = $proveedor->get_new_codigo();
       $proveedor->nombre = $_POST['nombre'];
@@ -89,9 +80,8 @@ class compras_proveedores extends fbase_controller
       $proveedor->cifnif = $_POST['cifnif'];
       $proveedor->acreedor = isset($_POST['acreedor']);
       $proveedor->personafisica = isset($_POST['personafisica']);
-      
-      if( $proveedor->save() )
-      {
+
+      if ($proveedor->save()) {
          $dirproveedor = new direccion_proveedor();
          $dirproveedor->codproveedor = $proveedor->codproveedor;
          $dirproveedor->descripcion = "Principal";
@@ -101,135 +91,112 @@ class compras_proveedores extends fbase_controller
          $dirproveedor->codpostal = $_POST['codpostal'];
          $dirproveedor->direccion = $_POST['direccion'];
          $dirproveedor->apartado = $_POST['apartado'];
-         
-         if( $dirproveedor->save() )
-         {
-            if($this->empresa->contintegrada)
-            {
+
+         if ($dirproveedor->save()) {
+            if ($this->empresa->contintegrada) {
                /// forzamos crear la subcuenta
                $proveedor->get_subcuenta($this->empresa->codejercicio);
             }
-            
+
             /// redireccionamos a la página del proveedor
-            header('location: '.$proveedor->url());
-         }
-         else
+            header('location: ' . $proveedor->url());
+         } else
             $this->new_error_msg("¡Imposible guardar la dirección el proveedor!");
-      }
-      else
+      } else
          $this->new_error_msg("¡Imposible guardar el proveedor!");
    }
-   
-   private function eliminar_proveedor()
-   {
+
+   private function eliminar_proveedor() {
       $proveedor = $this->proveedor->get($_GET['delete']);
-      if($proveedor)
-      {
-         if(FS_DEMO)
-         {
+      if ($proveedor) {
+         if (FS_DEMO) {
             $this->new_error_msg('En el modo demo no se pueden eliminar proveedores.
                Otros usuarios podrían necesitarlos.');
-         }
-         else if( !$this->allow_delete )
-         {
+         } else if (!$this->allow_delete) {
             $this->new_error_msg('No tienes permiso para eliminar en esta página.');
-         }
-         else if( $proveedor->delete() )
-         {
+         } else if ($proveedor->delete()) {
             $this->new_message('Proveedor eliminado correctamente.');
-         }
-         else
+         } else
             $this->new_error_msg('Ha sido imposible borrar el proveedor.');
-      }
-      else
+      } else
          $this->new_message('Proveedor no encontrado.');
    }
-   
-   private function buscar()
-   {
+
+   private function buscar() {
       $this->total_resultados = 0;
-      $query = mb_strtolower( $this->proveedor->no_html($this->query), 'UTF8' );
+      $query = mb_strtolower($this->proveedor->no_html($this->query), 'UTF8');
       $sql = " FROM proveedores";
       $and = ' WHERE ';
-      
-      if( is_numeric($query) )
-      {
-         $sql .= $and."(nombre LIKE '%".$query."%'"
-                 . " OR razonsocial LIKE '%".$query."%'"
-                 . " OR codproveedor LIKE '%".$query."%'"
-                 . " OR cifnif LIKE '%".$query."%'"
-                 . " OR telefono1 LIKE '".$query."%'"
-                 . " OR telefono2 LIKE '".$query."%'"
-                 . " OR observaciones LIKE '%".$query."%')";
+
+      if (is_numeric($query)) {
+         $sql .= $and . "(nombre LIKE '%" . $query . "%'"
+                 . " OR razonsocial LIKE '%" . $query . "%'"
+                 . " OR codproveedor LIKE '%" . $query . "%'"
+                 . " OR cifnif LIKE '%" . $query . "%'"
+                 . " OR telefono1 LIKE '" . $query . "%'"
+                 . " OR telefono2 LIKE '" . $query . "%'"
+                 . " OR observaciones LIKE '%" . $query . "%')";
          $and = ' AND ';
-      }
-      else
-      {
+      } else {
          $buscar = str_replace(' ', '%', $query);
-         $sql .= $and."(lower(nombre) LIKE '%".$buscar."%'"
-                 . " OR lower(razonsocial) LIKE '%".$buscar."%'"
-                 . " OR lower(cifnif) LIKE '%".$buscar."%'"
-                 . " OR lower(observaciones) LIKE '%".$buscar."%'"
-                 . " OR lower(email) LIKE '%".$buscar."%')";
+         $sql .= $and . "(lower(nombre) LIKE '%" . $buscar . "%'"
+                 . " OR lower(razonsocial) LIKE '%" . $buscar . "%'"
+                 . " OR lower(cifnif) LIKE '%" . $buscar . "%'"
+                 . " OR lower(observaciones) LIKE '%" . $buscar . "%'"
+                 . " OR lower(email) LIKE '%" . $buscar . "%')";
          $and = ' AND ';
       }
-      
-      if($this->tipo == 'acreedores')
-      {
-         $sql .= $and."acreedor = true";
+
+      if ($this->tipo == 'acreedores') {
+         $sql .= $and . "acreedor = true";
+         $and = ' AND ';
+      } else if ($this->tipo == 'noacreedores') {
+         $sql .= $and . "acreedor = false";
          $and = ' AND ';
       }
-      else if($this->tipo == 'noacreedores')
-      {
-         $sql .= $and."acreedor = false";
+
+      if ($this->debaja) {
+         $sql .= $and . "debaja = true";
+         $and = ' AND ';
+      } else {
+         $sql .= $and . "debaja = false";
          $and = ' AND ';
       }
-      
-      if($this->debaja)
-      {
-         $sql .= $and."debaja = true";
-         $and = ' AND ';
-      }
-      
-      $data = $this->db->select("SELECT COUNT(codproveedor) as total".$sql.';');
-      if($data)
-      {
+
+      $data = $this->db->select("SELECT COUNT(codproveedor) as total" . $sql . ';');
+      if ($data) {
          $this->num_resultados = intval($data[0]['total']);
-         
-         $data2 = $this->db->select_limit("SELECT *".$sql." ORDER BY ".$this->orden, FS_ITEM_LIMIT, $this->offset);
-         if($data2)
-         {
-            foreach($data2 as $d)
-            {
+
+         $data2 = $this->db->select_limit("SELECT *" . $sql . " ORDER BY " . $this->orden, FS_ITEM_LIMIT, $this->offset);
+         if ($data2) {
+            foreach ($data2 as $d) {
                $this->resultados[] = new proveedor($d);
             }
          }
       }
    }
-   
-   public function orden()
-   {
+
+   public function orden() {
       return array(
           'lower(nombre) ASC' => 'Orden: nombre',
           'lower(nombre) DESC' => 'Orden: nombre descendente',
-          'cifnif ASC' => 'Orden: '.FS_CIFNIF,
-          'cifnif DESC' => 'Orden: '.FS_CIFNIF.' descendente',
+          'cifnif ASC' => 'Orden: ' . FS_CIFNIF,
+          'cifnif DESC' => 'Orden: ' . FS_CIFNIF . ' descendente',
           'codproveedor ASC' => 'Orden: código',
           'codproveedor DESC' => 'Orden: código descendente'
       );
    }
 
-   public function paginas()
-   {
-      $url = $this->url()."&query=".$this->query
-              ."&tipo=".$this->tipo
-              ."&orden=".$this->orden;
-      
-      if($this->debaja)
-      {
+   public function paginas() {
+      $url = $this->url() . "&query=" . $this->query
+              . "&tipo=" . $this->tipo
+              . "&orden=" . $this->orden;
+
+      if ($this->debaja) {
          $url .= '&debaja=TRUE';
       }
-      
+
       return $this->fbase_paginas($url, $this->num_resultados, $this->offset);
    }
+
 }
