76abbcf97cd0470f446ebe0b71ab600c612bf11e
NeoRazorX <neorazorx@gmail.com>
2017-06-03 18:32:07 +0200

    Sustituidos los informes de clientes y proveedores por enlaces a los respectivos informes de albaranes y facturas.

--- controller/ventas_cliente.php
+++ controller/ventas_cliente.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of facturacion_base
  * Copyright (C) 2013-2017  Carlos Garcia Gomez  neorazorx@gmail.com
@@ -29,8 +30,8 @@ require_model('serie.php');
 require_model('cuenta_banco_proveedor.php');
 require_model('direccion_proveedor.php');
 
-class ventas_cliente extends fbase_controller
-{
+class ventas_cliente extends fbase_controller {
+
    public $agente;
    public $cliente;
    public $cuenta_banco;
@@ -39,16 +40,14 @@ class ventas_cliente extends fbase_controller
    public $grupo;
    public $pais;
    public $serie;
-   
-   public function __construct()
-   {
+
+   public function __construct() {
       parent::__construct(__CLASS__, 'Cliente', 'ventas', FALSE, FALSE);
    }
-   
-   protected function private_core()
-   {
+
+   protected function private_core() {
       parent::private_core();
-      
+
       $this->ppage = $this->page->get('ventas_clientes');
       $this->agente = new agente();
       $this->cuenta_banco = new cuenta_banco_cliente();
@@ -57,71 +56,48 @@ class ventas_cliente extends fbase_controller
       $this->grupo = new grupo_clientes();
       $this->pais = new pais();
       $this->serie = new serie();
-      
+
       /// cargamos el cliente
       $cliente = new cliente();
       $this->cliente = FALSE;
-      if( isset($_POST['codcliente']) )
-      {
-         $this->cliente = $cliente->get( $_POST['codcliente'] );
-      }
-      else if( isset($_GET['cod']) )
-      {
+      if (isset($_POST['codcliente'])) {
+         $this->cliente = $cliente->get($_POST['codcliente']);
+      } else if (isset($_GET['cod'])) {
          $this->cliente = $cliente->get($_GET['cod']);
       }
-      
-      if($this->cliente)
-      {
+
+      if ($this->cliente) {
          $this->page->title = $this->cliente->codcliente;
-         
+
          /// ¿Hay que hacer algo más?
-         if( isset($_GET['delete_cuenta']) ) /// eliminar cuenta bancaria
-         {
+         if (isset($_GET['delete_cuenta'])) { /// eliminar cuenta bancaria
             $this->delete_cuenta_banco();
-         }
-         else if( isset($_GET['delete_dir']) ) /// eliminar dirección
-         {
+         } else if (isset($_GET['delete_dir'])) { /// eliminar dirección
             $this->delete_direccion();
-         }
-         else if( isset($_POST['coddir']) ) /// añadir/modificar dirección
-         {
+         } else if (isset($_POST['coddir'])) { /// añadir/modificar dirección
             $this->edit_direccion();
-         }
-         else if( isset($_POST['iban']) ) /// añadir/modificar iban
-         {
+         } else if (isset($_POST['iban'])) { /// añadir/modificar iban
             $this->edit_cuenta_banco();
-         }
-         else if( isset($_POST['codcliente']) ) /// modificar cliente
-         {
+         } else if (isset($_POST['codcliente'])) { /// modificar cliente
             $this->modificar();
+         } else if (isset($_GET['convertir'])) {
+            $this->convertir();
          }
-         else if( isset($_GET['convertir']) ) 
-         {
-         	$this->convertir();
-         }
-      }
-      else
-      {
+      } else {
          $this->new_error_msg("¡Cliente no encontrado!", 'error', FALSE, FALSE);
       }
    }
-   
-   public function url()
-   {
-      if( !isset($this->cliente) )
-      {
+
+   public function url() {
+      if (!isset($this->cliente)) {
          return parent::url();
-      }
-      else if($this->cliente)
-      {
+      } else if ($this->cliente) {
          return $this->cliente->url();
-      }
-      else
+      } else
          return $this->ppage->url();
    }
-   
-   private function modificar()
-   {
+
+   private function modificar() {
       $this->cliente->nombre = $_POST['nombre'];
       $this->cliente->razonsocial = $_POST['razonsocial'];
       $this->cliente->tipoidfiscal = $_POST['tipoidfiscal'];
@@ -139,89 +115,69 @@ class ventas_cliente extends fbase_controller
       $this->cliente->debaja = isset($_POST['debaja']);
       $this->cliente->personafisica = isset($_POST['personafisica']);
       $this->cliente->diaspago = $_POST['diaspago'];
-      
+
       $this->cliente->codserie = NULL;
-      if($_POST['codserie'] != '')
-      {
+      if ($_POST['codserie'] != '') {
          $this->cliente->codserie = $_POST['codserie'];
       }
-      
+
       $this->cliente->codagente = NULL;
-      if($_POST['codagente'] != '')
-      {
+      if ($_POST['codagente'] != '') {
          $this->cliente->codagente = $_POST['codagente'];
       }
-      
+
       $this->cliente->codgrupo = NULL;
-      if($_POST['codgrupo'] != '')
-      {
+      if ($_POST['codgrupo'] != '') {
          $this->cliente->codgrupo = $_POST['codgrupo'];
       }
-      
-      if( $this->cliente->save() )
-      {
+
+      if ($this->cliente->save()) {
          $this->new_message("Datos del cliente modificados correctamente.");
          $this->propagar_cifnif();
-      }
-      else
+      } else
          $this->new_error_msg("¡Imposible modificar los datos del cliente!");
    }
-   
-   private function edit_cuenta_banco()
-   {
-      if( isset($_POST['codcuenta']) )
-      {
+
+   private function edit_cuenta_banco() {
+      if (isset($_POST['codcuenta'])) {
          $cuentab = $this->cuenta_banco->get($_POST['codcuenta']);
-      }
-      else
-      {
+      } else {
          $cuentab = new cuenta_banco_cliente();
          $cuentab->codcliente = $_POST['codcliente'];
       }
-      
+
       $cuentab->descripcion = $_POST['descripcion'];
       $cuentab->iban = $_POST['iban'];
       $cuentab->swift = $_POST['swift'];
       $cuentab->principal = isset($_POST['principal']);
       $cuentab->fmandato = NULL;
-      
-      if( isset($_POST['fmandato']) )
-      {
-         if($_POST['fmandato'] != '')
-         {
+
+      if (isset($_POST['fmandato'])) {
+         if ($_POST['fmandato'] != '') {
             $cuentab->fmandato = $_POST['fmandato'];
          }
       }
-      
-      if( $cuentab->save() )
-      {
+
+      if ($cuentab->save()) {
          $this->new_message('Cuenta bancaria guardada correctamente.');
-      }
-      else
+      } else
          $this->new_error_msg('Imposible guardar la cuenta bancaria.');
    }
-   
-   private function delete_cuenta_banco()
-   {
+
+   private function delete_cuenta_banco() {
       $cuenta = $this->cuenta_banco->get($_GET['delete_cuenta']);
-      if($cuenta)
-      {
-         if( $cuenta->delete() )
-         {
+      if ($cuenta) {
+         if ($cuenta->delete()) {
             $this->new_message('Cuenta bancaria eliminada correctamente.');
-         }
-         else
+         } else
             $this->new_error_msg('Imposible eliminar la cuenta bancaria.');
-      }
-      else
+      } else
          $this->new_error_msg('Cuenta bancaria no encontrada.');
    }
-   
-   private function edit_direccion()
-   {
+
+   private function edit_direccion() {
       $dir = new direccion_cliente();
-      if($_POST['coddir'] != '')
-      {
+      if ($_POST['coddir'] != '') {
          $dir = $dir->get($_POST['coddir']);
       }
       $dir->apartado = $_POST['apartado'];
@@ -234,33 +190,25 @@ class ventas_cliente extends fbase_controller
       $dir->domenvio = isset($_POST['direnvio']);
       $dir->domfacturacion = isset($_POST['dirfact']);
       $dir->provincia = $_POST['provincia'];
-      if( $dir->save() )
-      {
+      if ($dir->save()) {
          $this->new_message("Dirección guardada correctamente.");
-      }
-      else
+      } else
          $this->new_message("¡Imposible guardar la dirección!");
    }
-   
-   private function delete_direccion()
-   {
+
+   private function delete_direccion() {
       $dir = new direccion_cliente();
       $dir0 = $dir->get($_GET['delete_dir']);
-      if($dir0)
-      {
-         if( $dir0->delete() )
-         {
+      if ($dir0) {
+         if ($dir0->delete()) {
             $this->new_message('Dirección eliminada correctamente.');
-         }
-         else
+         } else
             $this->new_error_msg('Imposible eliminar la dirección.');
-      }
-      else
+      } else
          $this->new_error_msg('Dirección no encontrada.');
    }
-   
-   private function convertir()
-   {
+
+   private function convertir() {
       $proveedor = new proveedor();
       $proveedor->nombre = $this->cliente->nombre;
       $proveedor->razonsocial = $this->cliente->razonsocial;
@@ -278,183 +226,85 @@ class ventas_cliente extends fbase_controller
       $proveedor->personafisica = $this->cliente->personafisica;
       $proveedor->codserie = $this->cliente->codserie;
       $proveedor->codcliente = $this->cliente->codcliente;
-      
+
       $proveedor_ok = TRUE;
-      if( $proveedor->save() )
-      {
+      if ($proveedor->save()) {
          $this->cliente->codproveedor = $proveedor->codproveedor;
          $proveedor_ok = $this->cliente->save();
-      }
-      else
+      } else
          $proveedor_ok = FALSE;
-      
-      if($proveedor_ok)
-      {
+
+      if ($proveedor_ok) {
          /* cuentas de banco */
-         foreach($this->cuenta_banco->all_from_cliente($this->cliente->codcliente) as $c)
-	      {
+         foreach ($this->cuenta_banco->all_from_cliente($this->cliente->codcliente) as $c) {
             $c_banco_proveedor = new cuenta_banco_proveedor();
             $c_banco_proveedor->codproveedor = $proveedor->codproveedor;
             $c_banco_proveedor->descripcion = $c->descripcion;
             $c_banco_proveedor->iban = $c->iban;
             $c_banco_proveedor->swift = $c->swift;
             $c_banco_proveedor->save();
-	      }
-         
+         }
+
          /* direcciones */
-	      foreach($this->cliente->get_direcciones() as $d)
-	      {
+         foreach ($this->cliente->get_direcciones() as $d) {
             $direcc_pro = new direccion_proveedor();
-	         $direcc_pro->codproveedor = $proveedor->codproveedor;
-	         $direcc_pro->codpais = $d->codpais;
-	         $direcc_pro->apartado = $d->apartado;
-	         $direcc_pro->provincia = $d->provincia;
-	         $direcc_pro->ciudad = $d->ciudad;
-	         $direcc_pro->codpostal = $d->codpostal;
-	         $direcc_pro->direccion = $d->direccion;
+            $direcc_pro->codproveedor = $proveedor->codproveedor;
+            $direcc_pro->codpais = $d->codpais;
+            $direcc_pro->apartado = $d->apartado;
+            $direcc_pro->provincia = $d->provincia;
+            $direcc_pro->ciudad = $d->ciudad;
+            $direcc_pro->codpostal = $d->codpostal;
+            $direcc_pro->direccion = $d->direccion;
             $direcc_pro->descripcion = $d->descripcion;
             $direcc_pro->save();
-	      }
-         
+         }
+
          $this->new_message('Proveedor creado correctamente.');
-      }
-      else
+      } else
          $this->new_error_msg("¡Imposible crear el proveedor!");
    }
-   
-   /*
-    * Devuelve un array con los datos estadísticos de las compras del cliente
-    * en los cinco últimos años.
-    */
-   public function stats_from_cli()
-   {
-      $stats = array();
-      $years = array();
-      for($i=4; $i>=0; $i--)
-      {
-         $years[] = intval(Date('Y')) - $i;
-      }
-      
-      $meses = array('Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic');
-      
-      foreach($years as $year)
-      {
-         if( $year == intval(Date('Y')) )
-         {
-            /// año actual
-            for($i = 1; $i <= intval(Date('m')); $i++)
-            {
-               $stats[$year.'-'.$i]['mes'] = $meses[$i-1].' '.$year;
-               $stats[$year.'-'.$i]['albaranes'] = 0;
-               $stats[$year.'-'.$i]['facturas'] = 0;
-            }
-         }
-         else
-         {
-            /// años anteriores
-            for($i = 1; $i <= 12; $i++)
-            {
-               $stats[$year.'-'.$i]['mes'] = $meses[$i-1].' '.$year;
-               $stats[$year.'-'.$i]['albaranes'] = 0;
-               $stats[$year.'-'.$i]['facturas'] = 0;
-            }
-         }
-         
-         if( strtolower(FS_DB_TYPE) == 'postgresql')
-         {
-            $sql_aux = "to_char(fecha,'FMMM')";
-         }
-         else
-            $sql_aux = "DATE_FORMAT(fecha, '%m')";
-         
-         $sql = "SELECT ".$sql_aux." as mes, sum(neto/tasaconv) as total FROM albaranescli"
-                 ." WHERE fecha >= ".$this->empresa->var2str(Date('1-1-'.$year))
-                 ." AND fecha <= ".$this->empresa->var2str(Date('31-12-'.$year))
-                 ." AND codcliente = ".$this->empresa->var2str($this->cliente->codcliente)
-                 ." GROUP BY mes ORDER BY mes ASC;";
-         
-         $data = $this->db->select($sql);
-         if($data)
-         {
-            foreach($data as $d)
-            {
-               if( isset($stats[$year.'-'.intval($d['mes'])]['albaranes']) )
-               {
-                  $total = $this->euro_convert( floatval($d['total']) );
-                  $stats[$year.'-'.intval($d['mes'])]['albaranes'] = number_format($total, FS_NF0, '.', '');
-               }
-            }
-         }
-         
-         $sql = "SELECT ".$sql_aux." as mes, sum(neto/tasaconv) as total FROM facturascli"
-                 ." WHERE fecha >= ".$this->empresa->var2str(Date('1-1-'.$year))
-                 ." AND fecha <= ".$this->empresa->var2str(Date('31-12-'.$year))
-                 ." AND codcliente = ".$this->empresa->var2str($this->cliente->codcliente)
-                 ." GROUP BY mes ORDER BY mes ASC;";
-         $data = $this->db->select($sql);
-         if($data)
-         {
-            foreach($data as $d)
-            {
-               if( isset($stats[$year.'-'.intval($d['mes'])]['facturas']) )
-               {
-                  $total = $this->euro_convert( floatval($d['total']) );
-                  $stats[$year.'-'.intval($d['mes'])]['facturas'] = number_format($total, FS_NF0, '.', '');
-               }
-            }
-         }
-      }
-      
-      return $stats;
-   }
-   
-   public function tiene_facturas()
-   {
+
+   public function tiene_facturas() {
       $tiene = FALSE;
-      
-      if( $this->db->table_exists('facturascli') )
-      {
-         $sql = "SELECT * FROM facturascli WHERE codcliente = ".$this->cliente->var2str($this->cliente->codcliente);
-         
+
+      if ($this->db->table_exists('facturascli')) {
+         $sql = "SELECT * FROM facturascli WHERE codcliente = " . $this->cliente->var2str($this->cliente->codcliente);
+
          $data = $this->db->select_limit($sql, 5, 0);
-         if($data)
-         {
+         if ($data) {
             $tiene = TRUE;
          }
       }
-      
-      if( !$tiene AND $this->db->table_exists('albaranescli') )
-      {
-         $sql = "SELECT * FROM albaranescli WHERE codcliente = ".$this->cliente->var2str($this->cliente->codcliente);
-         
+
+      if (!$tiene AND $this->db->table_exists('albaranescli')) {
+         $sql = "SELECT * FROM albaranescli WHERE codcliente = " . $this->cliente->var2str($this->cliente->codcliente);
+
          $data = $this->db->select_limit($sql, 5, 0);
-         if($data)
-         {
+         if ($data) {
             $tiene = TRUE;
          }
       }
-      
+
       return $tiene;
    }
-   
+
    /**
     * Asigna el cif/nif a todos los albaranes y facturas del cliente que no tengan cif/nif
     */
-   private function propagar_cifnif()
-   {
-      if($this->cliente->cifnif)
-      {
+   private function propagar_cifnif() {
+      if ($this->cliente->cifnif) {
          /// actualizamos albaranes
-         $sql = "UPDATE albaranescli SET cifnif = ".$this->cliente->var2str($this->cliente->cifnif)
-                 ." WHERE codcliente = ".$this->cliente->var2str($this->cliente->codcliente)
-                 ." AND cifnif = '' AND fecha >= ".$this->cliente->var2str( date('01-01-Y') ).";";
+         $sql = "UPDATE albaranescli SET cifnif = " . $this->cliente->var2str($this->cliente->cifnif)
+                 . " WHERE codcliente = " . $this->cliente->var2str($this->cliente->codcliente)
+                 . " AND cifnif = '' AND fecha >= " . $this->cliente->var2str(date('01-01-Y')) . ";";
          $this->db->exec($sql);
-         
+
          /// actualizamos facturas
-         $sql = "UPDATE facturascli SET cifnif = ".$this->cliente->var2str($this->cliente->cifnif)
-                 ." WHERE codcliente = ".$this->cliente->var2str($this->cliente->codcliente)
-                 ." AND cifnif = '' AND fecha >= ".$this->cliente->var2str( date('01-01-Y') ).";";
+         $sql = "UPDATE facturascli SET cifnif = " . $this->cliente->var2str($this->cliente->cifnif)
+                 . " WHERE codcliente = " . $this->cliente->var2str($this->cliente->codcliente)
+                 . " AND cifnif = '' AND fecha >= " . $this->cliente->var2str(date('01-01-Y')) . ";";
          $this->db->exec($sql);
       }
    }
+
 }
